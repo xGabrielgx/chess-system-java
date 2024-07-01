b4231f9b224b527b4d64aa632edb7a587d7c2ec0
Gabriel Xavier <gabrielgx928@gmail.com>
2024-07-01 11:36:05 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,7 +33,13 @@ public class Board {
         return pieces[row][column];
     }
 
+    // downcasting / sobrecarga
     public Piece piece(Position position) {
         return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
     }
 }
